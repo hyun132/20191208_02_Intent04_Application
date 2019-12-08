9c77c3ba2b2aa9e06e36730157b40dd10f57015f
미현 김 <rlaalgus132@naver.com>
2019-12-08 11:39:44 +0900

    전화 연결 => 통화 버튼 직전(DIAL) 인텐트 연습

--- app/src/main/java/com/tjoeun/a20191208_02_intent04_application/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191208_02_intent04_application/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.tjoeun.a20191208_02_intent04_application
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dialBtn.setOnClickListener {
+//            누구에게 전화를 걸지 정보를 미리 명시.
+            val uri = Uri.parse("tel:01051123237")
+//            어느 화면에 넘겨줄지 명시
+            val intent = Intent(Intent.ACTION_DIAL,uri)
+            startActivity(intent)
+        }
+
     }
 }
